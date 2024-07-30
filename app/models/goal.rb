488e03e9871902ded7a9d74ed8111cfcd0038a6a
stewartoliver <101909671+stewartoliver@users.noreply.github.com>
2024-07-31 00:47:16 +1200

    goal progress
    based off task completion*

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,4 +3,11 @@ class Goal < ApplicationRecord
 	has_many :sub_goals, class_name: "Goal", foreign_key: "parent_goal_id", dependent: :destroy
 	belongs_to :parent_goal, class_name: "Goal", optional: true
 	has_many :tasks, dependent: :destroy
+
+	def update_progress
+		total_tasks = tasks.count
+		completed_tasks = tasks.done.count
+		self.progress = total_tasks > 0 ? (completed_tasks.to_f / total_tasks * 100).round : 0
+		save
+	end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,12 @@ class Task < ApplicationRecord
 
   validates :title, presence: true
   enum status: { todo: 0, in_progress: 1, done: 2 }
+
+  after_save :update_goal_progress
+
+  private
+
+  def update_goal_progress
+    goal.update_progress
+  end
 end
